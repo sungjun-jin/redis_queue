84b8ca296fc9074b85cab71d8312a138f8efc5c1
sungjun-jin <sungjun@peoplefund.co.kr>
2021-02-08 16:34:07 +0900

    signiture test commit

--- message/views.py
+++ message/views.py
@@ -5,17 +5,20 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK
 from .redis import RedisQueue
-from .exceptions import CustomException
 
 logger = logging.getLogger(__name__)
 
 
 @api_view(['POST'])
-def test(request):
-    data = json.loads(request.body)
-    key = data['key']
-    value = data['value']
-    redis = RedisQueue()
-    redis.enqueue(key,value)
-    response = redis.dequeue(key)
-    return Response({"data": response}, status=HTTP_200_OK)
+def post_data(request):
+    try:
+        # args (key, value)
+        data = json.loads(request.body)
+        redis = RedisQueue()
+        redis.enqueue(data['key'], data['value'])
+        return Response(status=HTTP_200_OK)
+
+    except Exception as e:
+        return Response({"message": str(e)}, status=400)
+
+
