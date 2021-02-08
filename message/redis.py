79b7bde538e3ac10d2ad5f99228347cc283f6052
Sung Jun Jin <sungjun@peoplefund.co.kr>
2021-02-08 16:52:23 +0900

    test gitclient verified commit

--- message/redis.py
+++ message/redis.py
@@ -1,6 +1,7 @@
 import redis
-import os
 import logging
+
+from .exceptions import *
 
 logger = logging.getLogger(__name__)
 
@@ -8,11 +9,11 @@ logger = logging.getLogger(__name__)
 class RedisQueue():
     def __init__(self):
         self.redis = redis.StrictRedis(
-            host = "localhost",
-            port = 6379
+            host="localhost",
+            port=6379
         )
 
-        logger.info("redis_init")
+        logger.info("redis_initialization")
 
     def enqueue(self, key, value):
         self.redis.rpush(key, value)
